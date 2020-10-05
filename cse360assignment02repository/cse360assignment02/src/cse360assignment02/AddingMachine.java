4a38efda4cf5ae7707ae7139dafb0bc2814c3013
Jason <jljudge@asu.edu>
2020-10-05 15:07:23 -0700

    Initial blank methods with comments added

---
+++ cse360assignment02repository/cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,50 @@
+/**
+ * Description: Performs basic addition and subtraction operations, prints both operations and result
+ * @author Jason Judge
+ */
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+ /**
+  * Constructor of AddingMachine
+  */
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+ /**
+  * Returns the total of the machine
+  * @return total of calculations
+  */
+  public int getTotal () {
+    return 0;
+  }
+ /**
+  * Adds a value to the total
+  * @param number that will be added 
+  */
+  public void add (int value) {
+  }
+  
+ /**
+  * Subtracts a value to the total
+  * @param number that will be subtracted 
+  */
+  public void subtract (int value) {
+  }
+
+ /**
+  * Returns a string that shows the operations and final answer
+  * @return returns the result of operations and every operation
+  */
+  public String toString () {
+    return "";
+  }
+
+ /**
+  * Resets the total and operation string
+  */
+  public void clear() {
+  }
+}
