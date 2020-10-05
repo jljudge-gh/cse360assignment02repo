b1d8b52ee139ea1cedb354ec629f7c742579b526
Jason <jljudge@asu.edu>
2020-10-05 15:54:20 -0700

    Added a Main for testing purposes

--- cse360assignment02repository/cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02repository/cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -55,4 +55,15 @@ public class AddingMachine {
 	  total = 0;
 	  operations.delete(0, operations.length());
   }
+  
+ /**
+  * Main for testing purposes
+  */
+  public static void main(String[] args) {
+	    AddingMachine myCalculator = new AddingMachine();
+	    myCalculator.add(4); 
+	    myCalculator.subtract(2); 
+	    myCalculator.add(5);
+	    System.out.println( myCalculator.toString() );
+	}
 }
